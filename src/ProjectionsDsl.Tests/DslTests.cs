13f2fc8dce443d41b866dbef22f6d8b345ed3ba2
Joseph Flood <joseph.flood@hotmail.com>
2013-08-06 21:41:02 +1000

    first commit - dsl for projections inspired by the event store

---
+++ src/ProjectionsDsl.Tests/DslTests.cs
@@ -0,0 +1,187 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ProjectionsDsl.Tests
+{
+    public class DslTests
+    {
+        [Theory, AutoMoqData]
+        public void ShouldExecuteWhenForEvent(
+            FakeProjectionSource source, FakeDocumentWriter writer, TestEvent @event)
+        {
+            writer.DB.Clear();
+            var projection = Streams.Configure()
+                 .WithEventSource(source)
+                 .WithProjectionWriter(writer)
+                 .Build();
+
+            projection.FromAll()
+                .Init(() => new ListOfProjectedState())
+                .When<TestEvent>((e, s) =>
+                    s.AddState(
+                        new TestProjectionState(e.SomeState)),
+                    e => e.Id);
+
+            source.Events.OnNext(@event);
+
+            writer.DB.Select(x => x.Value).ShouldContain<ListOfProjectedState>();
+        }
+
+        [Theory, AutoMoqData]
+        public void ShouldNotExecuteWhenForEventsNotInterestedIn(
+            FakeProjectionSource source, FakeDocumentWriter writer, NotInterestedInThis @event)
+        {
+            writer.DB.Clear();
+            var projection = Streams.Configure()
+                 .WithEventSource(source)
+                 .WithProjectionWriter(writer)
+                 .Build();
+
+            projection.FromAll()
+                .Init(() => new ListOfProjectedState())
+                .When<TestEvent>((e, s) =>
+                    s.AddState(
+                        new TestProjectionState(e.SomeState)),
+                    e => e.Id);
+
+            source.Events.OnNext(@event);
+
+            writer.DB.Select(x => x.Value).ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void ConfigureProjectionSourceAndWriter()
+        {
+            var projection = Streams.Configure()
+                .WithEventSource(new FakeProjectionSource())
+                .WithProjectionWriter(new FakeDocumentWriter())
+                .Build();
+
+            Assert.IsType(typeof(FakeProjectionSource), projection.Source);
+            Assert.IsType(typeof(FakeDocumentWriter), projection.Writer);
+        }
+
+        public class FakeProjectionSource : IProjectionSource
+        {
+            public Subject<IEvent> Events { get; set; }
+        }
+
+        public class FakeDocumentWriter : IDocumentWriter<Guid, ListOfProjectedState>
+        {
+            public Dictionary<Guid, ListOfProjectedState> DB = new Dictionary<Guid, ListOfProjectedState>();
+            public ListOfProjectedState AddOrUpdate(Guid key, Func<ListOfProjectedState> addFactory, Func<ListOfProjectedState, ListOfProjectedState> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists)
+            {
+                ListOfProjectedState document;
+                if (DB.ContainsKey(key))
+                {
+                    document = DB[key];
+                    update(document);
+                }
+                else
+                {
+                    document = addFactory();
+                    DB.Add(key, document);
+                }
+                return document;
+            }
+
+            public bool TryDelete(Guid key)
+            {
+                DB.Remove(key);
+                return true;
+            }
+        }
+
+        public class NotInterestedInThis : IEvent
+        {
+
+        }
+
+
+        public class ListOfProjectedState
+        {
+            public List<TestProjectionState> Events = new List<TestProjectionState>();
+
+            public ListOfProjectedState AddState(TestProjectionState state)
+            {
+                Events.Add(state);
+                return this;
+            }
+        }
+
+        public class TestEvent : IEvent
+        {
+            public TestEvent(Guid id)
+            {
+                Id = id;
+            }
+
+            public Guid Id { get; set; }
+            public string SomeState { get; set; }
+        }
+
+        public class TestProjectionState
+        {
+            public TestProjectionState(string someState)
+            {
+                SomeState = someState;
+            }
+
+            public string SomeState { get; set; }
+        }
+    }
+
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() :
+            base(new Fixture().Customize(new AutoMoqCustomization()))
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Assert (throw exceptions) 
+    /// </summary>
+    public static class ShouldAssertions
+    {
+        public static void ShouldContainOnly<T>(this IEnumerable<object> messages) where T : class
+        {
+            ShouldContainOnly<T>(messages, null);
+        }
+
+        public static void ShouldContainOnly<T>(this IEnumerable<object> messages, Func<T, bool> isExpected) where T : class
+        {
+            Assert.Equal(1, messages.Count());
+            ShouldContain(messages, isExpected);
+        }
+
+        public static void ShouldContain<T>(this IEnumerable<object> messages)
+            where T : class
+        {
+            ShouldContain<T>(messages, null);
+        }
+
+        public static void ShouldContain<T>(this IEnumerable<object> messages, Func<T, bool> isExpected) where T : class
+        {
+            Assert.True(messages.Any(m =>
+            {
+                return m is T && (isExpected == null || isExpected(m as T));
+            }));
+        }
+
+        public static void ShouldBeEmpty(this IEnumerable<object> messages)
+        {
+            Assert.Empty(messages);
+        }
+    }
+}
---
+++ src/ProjectionsDsl/Streams.cs
@@ -0,0 +1,224 @@
+﻿using ProjectionsDsl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectionsDsl
+{
+    public class Streams
+    {
+        public static ConfigureEventSource Configure()
+        {
+            return new ConfigureEventSource();
+        }
+
+        public class ConfigureEventSource
+        {
+            public ConfigureWriter WithEventSource(IProjectionSource source)
+            {
+                return new ConfigureWriter(source);
+            }
+
+            public class ConfigureWriter
+            {
+                private IProjectionSource source;
+
+                public ConfigureWriter(IProjectionSource source)
+                {
+                    this.source = source;
+                }
+
+                public ConfigureProjectionBuilder<TState> WithProjectionWriter<TState>(IDocumentWriter<Guid, TState> writer)
+                {
+                    return new ConfigureProjectionBuilder<TState>(this.source, writer);
+                }
+
+                public class ConfigureProjectionBuilder<TState>
+                {
+                    private IProjectionSource projectionSource;
+                    private IDocumentWriter<Guid, TState> writer;
+
+                    public ConfigureProjectionBuilder(IProjectionSource projectionSource, IDocumentWriter<Guid, TState> writer)
+                    {
+                        this.projectionSource = projectionSource;
+                        this.writer = writer;
+                    }
+                    public ProjectionBuilder<TState> Build()
+                    {
+                        return new ProjectionBuilder<TState>(projectionSource, writer);
+                    }
+                }
+            }
+        }
+    }
+
+    public class ProjectionBuilder<TState>
+    {
+        public IProjectionSource Source { get; private set; }
+        public IDocumentWriter<Guid, TState> Writer { get; private set; }
+
+        public ProjectionBuilder(IProjectionSource projectionSource, IDocumentWriter<Guid, TState> writer)
+        {
+            this.Source = projectionSource;
+            this.Writer = writer;
+        }
+
+        public IInitialiseProjectionState<TState> FromAll()
+        {
+            return new ProjectionAcrossAllStreams<TState>(this.Source, this.Writer);
+        }
+    }
+
+    public class ProjectionAcrossAllStreams<TState> : IInitialiseProjectionState<TState>
+    {
+        private IProjectionSource source;
+        private IDocumentWriter<Guid, TState> writer;
+
+        public ProjectionAcrossAllStreams(IProjectionSource projectionSource, IDocumentWriter<Guid, TState> documentWriter)
+        {
+            this.source = projectionSource;
+            this.writer = documentWriter;
+        }
+        public IProjection<TState> Init(Func<TState> init)
+        {
+            return new DocumentWriterProjection<TState>(source, writer, init);
+        }
+    }
+
+    public class DocumentWriterProjection<TState> : IProjection<TState>, IObserver<IEvent>
+    {
+        private List<Tuple<Func<IEvent, Guid>, Func<IEvent, TState, TState>>> hash = new List<Tuple<Func<IEvent, Guid>, Func<IEvent, TState, TState>>>();
+
+        private IProjectionSource source;
+        private IDocumentWriter<Guid, TState> writer;
+        private Func<TState> init;
+
+        public DocumentWriterProjection(IProjectionSource source, IDocumentWriter<Guid, TState> writer, Func<TState> init)
+        {
+            this.source = source;
+            this.writer = writer;
+            this.init = init;
+        }
+
+        public IProjection<TState> When<TEvent>(Expression<Func<TEvent, TState, TState>> handler, Expression<Func<TEvent, Guid>> id) where TEvent : IEvent
+        {
+            var idhandler = DelegateAdjuster.CastArgument(id);
+            var handlerFunc = DelegateAdjuster.CastArgument(handler);
+            source.Events.Where(x => x.GetType() == typeof(TEvent)).Subscribe(e =>
+                this.writer.AddOrUpdate(idhandler(e), init, state => handlerFunc(e, state)));
+            return this;
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void OnCompleted()
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {
+
+        }
+
+        public void OnNext(IEvent value)
+        {
+            foreach (var t in hash)
+            {
+                Func<IEvent, Guid> id = t.Item1;
+                Func<IEvent, TState, TState> when = t.Item2;
+                Func<TState, TState> update = state => when(value, state);
+                this.writer.AddOrUpdate(id(value), init, update);
+            }
+        }
+    }
+
+    public class DelegateAdjuster
+    {
+        public static Func<IEvent, TState, TState> CastArgument<TEvent, TState>(Expression<Func<TEvent, TState, TState>> source) where TEvent : IEvent
+        {
+            if (typeof(TEvent) == typeof(IEvent))
+            {
+                return (Func<IEvent, TState, TState>)((Delegate)source.Compile());
+
+            }
+            var eventParameter = Expression.Parameter(typeof(IEvent), "source");
+            var stateParameter = Expression.Parameter(typeof(TState));
+            var converted = Expression.Convert(eventParameter, typeof(TEvent));
+            var invocation = Expression.Invoke(
+                    source,
+                    converted,
+                    stateParameter);
+            var result = Expression.Lambda<Func<IEvent, TState, TState>>(
+                invocation,
+                eventParameter, stateParameter);
+
+            return result.Compile();
+        }
+
+        public static Func<IEvent, Guid> CastArgument<TEvent>(Expression<Func<TEvent, Guid>> source) where TEvent : IEvent
+        {
+            if (typeof(TEvent) == typeof(IEvent))
+            {
+                return (Func<IEvent, Guid>)((Delegate)source.Compile());
+
+            }
+            var eventParameter = Expression.Parameter(typeof(IEvent), "source");
+            var converted = Expression.Convert(eventParameter, typeof(TEvent));
+            var invocation = Expression.Invoke(
+                    source,
+                    converted);
+            var result = Expression.Lambda<Func<IEvent, Guid>>(
+                invocation,
+                eventParameter);
+
+            return result.Compile();
+        }
+    }
+
+    public interface IProjectionSource
+    {
+        Subject<IEvent> Events { get; set; }
+    }
+
+
+    public interface IInitialiseProjectionState<TState>
+    {
+        IProjection<TState> Init(Func<TState> init);
+    }
+
+    public interface IProjection<TState>
+    {
+        IProjection<TState> When<TEvent>(Expression<Func<TEvent, TState, TState>> handler, Expression<Func<TEvent, Guid>> id) where TEvent : IEvent;
+    }
+
+    /// <summary>
+    /// This is a sample of strongly-typed document writer interface,
+    /// which works good for building simple systems that can be migrated
+    /// between cloud and various on-premises implementations.
+    /// This interface supports automated view rebuilding (which is
+    /// demonstrated in greater detail in Lokad.CQRS project)
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    public interface IDocumentWriter<in TKey, TEntity>
+    {
+        TEntity AddOrUpdate(TKey key, Func<TEntity> addFactory, Func<TEntity, TEntity> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists);
+        bool TryDelete(TKey key);
+    }
+
+    public enum AddOrUpdateHint
+    {
+        ProbablyExists,
+        ProbablyDoesNotExist
+    }
+
+}
